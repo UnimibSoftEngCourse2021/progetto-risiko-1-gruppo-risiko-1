6563bc4b8eb2dd0ca6fec405ffe32fec26a3589c
Francesco Regonesi <f.regonesi1@campus.unimib.it>
2021-01-03 13:21:07 +0100

    Added some frontend dependencies

--- src/main/javascript/risiko-frontend/src/main.js
+++ src/main/javascript/risiko-frontend/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
--- src/main/javascript/risiko-frontend/vue.config.js
+++ src/main/javascript/risiko-frontend/vue.config.js
@@ -1,6 +1,6 @@
-/**
- * Specify the output directory of "npm run build" command to be inside the maven "target" folder
- */
 module.exports = {
-    outputDir: "../../../../target/frontend/public"
+  "outputDir": "../../../../target/frontend/public",
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
