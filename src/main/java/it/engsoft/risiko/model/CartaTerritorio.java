dd4fe5794459baf0a9b81852508c0f572741f8d4
Riccardo Sartori <r.sartori2@campus.unimib.it>
2021-01-04 16:45:19 +0100

    Aggiunto CarteTerritorioService

--- src/main/java/it/engsoft/risiko/model/CartaTerritorio.java
+++ src/main/java/it/engsoft/risiko/model/CartaTerritorio.java
@@ -1,7 +1,7 @@
 package it.engsoft.risiko.model;
 
 public class CartaTerritorio {
-    private Stato statoRappresentato = null;
+    private final Stato statoRappresentato;
     private Figura figura;
 
     public enum Figura {
@@ -11,15 +11,13 @@ public class CartaTerritorio {
         JOLLY
     };
 
+    public CartaTerritorio(Stato statoRappresentato) {
+        this.statoRappresentato = statoRappresentato;
+    }
+
     // stato rappresentato
     public Stato getStatoRappresentato() {
         return statoRappresentato;
-    }
-
-    public void setStatoRappresentato(Stato statoRappresentato) {
-        if (statoRappresentato == null || figura.equals(Figura.JOLLY))
-            throw new RuntimeException("Stato non valido");
-        this.statoRappresentato = statoRappresentato;
     }
 
     // figura
@@ -32,47 +30,4 @@ public class CartaTerritorio {
             throw new RuntimeException("Figura nulla");
         this.figura = figura;
     }
-
-    // ritorna il valore di truppe che un giocatore può ricevere utilizzando un tris di carte
-    public int valoreTruppeTris(CartaTerritorio x, CartaTerritorio y, CartaTerritorio z) {
-        // le tre carte sono cannoni
-        if (x.figura == y.figura && y.figura == z.figura && z.figura == Figura.CANNONE)
-            return 4;
-        // le tre carte sono fanti
-        if (x.figura == y.figura && y.figura == z.figura && z.figura == Figura.FANTE)
-            return 6;
-        // le tre carte sono cavalli
-        if (x.figura == y.figura && y.figura == z.figura && z.figura == Figura.CAVALLO)
-            return 8;
-        // le tre carte sono composte da un jolly e due diverse tra loro
-        if (x.figura != y.figura && y.figura != z.figura && x.figura != z.figura && (x.figura == Figura.JOLLY || y.figura == Figura.JOLLY || z.figura == Figura.JOLLY))
-            return 10;
-        // le tre carte sono composte da un jolly e due uguali tra loro
-        if ((x.figura == y.figura && z.figura == Figura.JOLLY) || (x.figura == z.figura && y.figura == Figura.JOLLY) || (y.figura == z.figura && x.figura == Figura.JOLLY))
-            return 12;
-        // qualsiasi altra combinazione non è un tris valido e ritornerà zero
-        return 0;
-    }
-
-    // ritorna un extra se gli stati delle carte del tris appartengono al giocatore che le utilizza
-    public int extraTruppeTris(CartaTerritorio x, CartaTerritorio y, CartaTerritorio z) {
-        int risultato = 0;
-        for (int i = 0; i < x.statoRappresentato.getProprietario().getStati().size(); i++) {
-            if (x.statoRappresentato == x.statoRappresentato.getProprietario().getStati().get(i))
-                risultato = risultato + 2;
-            if (y.statoRappresentato == x.statoRappresentato.getProprietario().getStati().get(i))
-                risultato = risultato + 2;
-            if (z.statoRappresentato == x.statoRappresentato.getProprietario().getStati().get(i))
-                risultato = risultato + 2;
-            // i tre if sono da unire; provando a unirli mi dava problemi di sintassi ma, senza poter testare il tutto,
-            // per ora, restano cosi'
-        }
-        return risultato;
-    }
-
-    // ritorna il totale delle truppe ricevute per un dato tris di carte
-    public int TruppeTris(CartaTerritorio x, CartaTerritorio y, CartaTerritorio z) {
-        return valoreTruppeTris(x, y, z) + extraTruppeTris(x, y, z);
-    }
-
 }
---
+++ src/main/java/it/engsoft/risiko/service/CarteTerritorioService.java
@@ -0,0 +1,211 @@
+package it.engsoft.risiko.service;
+
+import it.engsoft.risiko.model.*;
+import it.engsoft.risiko.exceptions.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CarteTerritorioService {
+    private List<CartaTerritorio> mazzo = new ArrayList<>();
+    private int index; // indice per gestire il mazzo
+
+    /**
+     * Genera il mazzo e distribuisce le carte ai giocatori.
+     * Se il mazzo è già stato generato causa MossaIllegaleException.
+     */
+    public void distribuisciCarte(Partita partita) {
+        if (mazzo != null)
+            throw new MossaIllegaleException();
+
+        // generazione mazzo
+        generaMazzo(partita.getMappa());
+
+        // distribuzione carte ai giocatori
+        Collections.shuffle(mazzo);
+
+        int g = 0; // indice giocatori
+
+        // la carta 'c' viene assegnata al giocatore 'g'
+        for (CartaTerritorio cartaTerritorio : mazzo) {
+            // se la carta è un jolly passa alla carta successiva
+            if (cartaTerritorio.getFigura() == CartaTerritorio.Figura.JOLLY)
+                continue;
+
+            // Assegna lo stato rappresentato dalla carta al giocatore
+            partita.getGiocatori().get(g).aggiungiStato(cartaTerritorio.getStatoRappresentato());
+
+            if (g == partita.getGiocatori().size() - 1)
+                g = 0;
+            else
+                g++;
+        }
+
+        // Posiziona index prima della prima carta
+        index = -1;
+    }
+
+    /**
+     * Crea le carte e le aggiunge al mazzo.
+     *
+     * @param mappa: la mappa su cui si svolge la partita
+     */
+    private void generaMazzo(Mappa mappa) {
+        int numStati = 0;
+
+        // conta degli stati e creazione le carte
+        for (Continente continente : mappa.getContinenti()) {
+            for (Stato stato : continente.getStati()) {
+                numStati++;
+
+                mazzo.add(new CartaTerritorio(stato));
+            }
+        }
+
+        // aggiunta figure alle carte
+        for (int i = 0; i < mazzo.size(); i = i + 3) {
+            mazzo.get(i).setFigura(CartaTerritorio.Figura.CANNONE);
+            mazzo.get(i + 1).setFigura(CartaTerritorio.Figura.FANTE);
+            mazzo.get(i + 2).setFigura(CartaTerritorio.Figura.CAVALLO);
+        }
+
+        int numJolly = Math.round(numStati / 20F);
+
+        // aggiunge i jolly al mazzo
+        for (int i = 0; i < numJolly; i++) {
+            CartaTerritorio jolly = new CartaTerritorio(null);
+            jolly.setFigura(CartaTerritorio.Figura.JOLLY);
+
+            mazzo.add(jolly);
+        }
+    }
+
+    public CartaTerritorio pescaCarta(Giocatore giocatore) {
+        index++;
+
+        giocatore.aggiungiCartaTerritorio(mazzo.get(index));
+        return mazzo.get(index);
+    }
+
+    /**
+     * Calcola il numero di armate che spettano al giocatore che gioca il tris.
+     *
+     * @param giocatore : il giocatore che gioca il tris.
+     */
+    public int valutaTris(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c, Giocatore giocatore) {
+        int standard = truppeStandard(a, b, c);
+
+        // Se standard = 0 il tris non è valido
+        if (standard == 0)
+            throw new MossaIllegaleException();
+
+        // Rimette le carte usate nel mazzo
+        rimettiNelMazzo(a, b, c);
+
+        return standard + truppeExtra(a, b, c, giocatore);
+    }
+
+    /**
+     * Calcola il numero di truppe derivanti dalle figure del tris
+     */
+    private int truppeStandard(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c) {
+        // tris di cannoni
+        if (figureUguali(a, b, c) && a.getFigura() == CartaTerritorio.Figura.CANNONE)
+            return 4;
+
+        // tris di fanti
+        if (figureUguali(a, b, c) && a.getFigura() == CartaTerritorio.Figura.FANTE)
+            return 6;
+
+        // tris di cavalli
+        if (figureUguali(a, b, c) && a.getFigura() == CartaTerritorio.Figura.CAVALLO)
+            return 8;
+
+        // cannone - fante - cavallo
+        if (!coppiaUguale(a, b, c) && !figureUguali(a, b, c) && !jollyInTris(a, b, c))
+            return 10;
+
+        // jolly più due carte uguali tra loro
+        if (coppiaUguale(a, b, c) && jollyInTris(a, b, c))
+            return 12;
+
+        // qualsiasi altra combinazione non è un tris valido e ritornerà zero
+        return 0;
+    }
+
+    /**
+     * Se il giocatore possiede nella combinazione carte raffiguranti territori da lui occupati in quel momento,
+     * riceve 2 armate in più per ciascuna di queste carte.
+     */
+    private int truppeExtra(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c, Giocatore giocatore) {
+        int truppe = 0;
+
+        if (a.getStatoRappresentato() != null && giocatore.getStati().contains(a.getStatoRappresentato()))
+            truppe += 2;
+
+        if (b.getStatoRappresentato() != null && giocatore.getStati().contains(b.getStatoRappresentato()))
+            truppe += 2;
+
+        if (c.getStatoRappresentato() != null && giocatore.getStati().contains(c.getStatoRappresentato()))
+            truppe += 2;
+
+        return truppe;
+    }
+
+    /**
+     * Controlla se tre carte territorio hanno la stessa figura.
+     */
+    private boolean figureUguali(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c) {
+        return a.getFigura() == b.getFigura() && b.getFigura() == c.getFigura();
+    }
+
+    /**
+     * Controlla se due carte hanno la stessa figura e la terza ha una figura diversa.
+     */
+    private boolean coppiaUguale(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c) {
+        if (a.getFigura() == b.getFigura() && b.getFigura() != c.getFigura())
+            return true;
+
+        if (a.getFigura() == c.getFigura() && c.getFigura() != b.getFigura())
+            return true;
+
+        if (b.getFigura() == b.getFigura() && b.getFigura() != a.getFigura())
+            return true;
+
+        return false;
+    }
+
+    /**
+     * Controlla se tra le carte territorio è presente un jolly.
+     */
+    private boolean jollyInTris(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c) {
+        if (a.getFigura() == CartaTerritorio.Figura.JOLLY)
+            return true;
+
+        if (b.getFigura() == CartaTerritorio.Figura.JOLLY)
+            return true;
+
+        if (c.getFigura() == CartaTerritorio.Figura.JOLLY)
+            return true;
+
+        return false;
+    }
+
+    private void rimettiNelMazzo(CartaTerritorio a, CartaTerritorio b, CartaTerritorio c) {
+        // Rimuove le carte dal mazzo
+        mazzo.remove(a);
+        mazzo.remove(b);
+        mazzo.remove(c);
+
+        // Reinserisce le carte in fondo al mazzo
+        mazzo.add(a);
+        mazzo.add(b);
+        mazzo.add(c);
+
+        // Riduce index per rispettare le operazioni eseguite
+        index -= 3;
+    }
+}
