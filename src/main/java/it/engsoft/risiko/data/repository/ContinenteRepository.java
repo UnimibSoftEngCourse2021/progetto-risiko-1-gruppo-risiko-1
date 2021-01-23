bb06b2515c823fcc3a9cd6e7396f606c044dd385
RusconiD <d.rusconi9@campus.unimib.it>
2021-01-23 14:15:18 +0100

    Rimossi repository inutili

--- src/main/java/it/engsoft/risiko/data/repository/ContinenteRepository.java
+++
@@ -1,7 +0,0 @@
-package it.engsoft.risiko.data.repository;
-
-import it.engsoft.risiko.data.model.Continente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContinenteRepository extends CrudRepository<Continente, Long> {
-}
--- src/main/java/it/engsoft/risiko/data/repository/StatoRepository.java
+++
@@ -1,7 +0,0 @@
-package it.engsoft.risiko.data.repository;
-
-import it.engsoft.risiko.data.model.Stato;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StatoRepository extends CrudRepository<Stato, Long> {
-}
